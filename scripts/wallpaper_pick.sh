8ff05a3a858ec9b77f511cb81caf7a70f2e628d1
Rafael Olegário <rafaelolegario2008@gmail.com>
2025-12-13 17:01:10 -0300

    select wallpaper menu

---
+++ scripts/wallpaper_pick.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Diretório onde seus wallpapers estão
+WALL_DIR="/home/rafa/Imagens/wallpapers"
+
+# Pega a lista de arquivos normal (só nomes)
+choice=$(ls "$WALL_DIR" | wofi --dmenu --prompt "Pesquisar Wallpaper")
+
+# Se escolheu algo, aplica
+if [ -n "$choice" ]; then
+    swww img "$WALL_DIR/$choice" --transition-type grow --transition-duration 0.7
+fi
